66e761942e3b156912dc6380ca16f2c333980ab0
David Onoh <daveeddrumz@gmail.com>
2022-01-21 15:39:02 +0100

    created the tab bar

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_point_tab_bar/pointTabIndicator.dart';
 import 'package:twinku_blog/models/data.dart';
 import 'package:twinku_blog/views/breaking_news_scroll.dart';
 
@@ -10,127 +11,151 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  PageController? _pageController;
+  late TabController _tabController;
+  final tabList = ['All', 'information','Sports','Business','Culture'];
 
- PageController ?_pageController;
- @override
+  @override
   void initState() {
-   _pageController = PageController(initialPage: 2,viewportFraction: 0.8);
+    _pageController = PageController(initialPage: 2, viewportFraction: 0.8);
+    _tabController = TabController(vsync: this, length: tabList.length);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: 20.0,
-          // right: 20.0,
-          top: 25.0,
-        ),
-        child: ListView(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'Hello Sonia  👋',
-                      style: TextStyle(
-                          color: Color(0xFF0A1330),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: ClipOval(
-                        child: SizedBox.fromSize(
-                          size: Size.fromRadius(20), // Image radius
-                          child: Image.asset(
-                            'images/ff.jpeg',
-                            scale: 35,
-                            fit: BoxFit.cover,
+    return DefaultTabController(
+      length: tabList.length,
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(
+            left: 20.0,
+            // right: 20.0,
+            top: 25.0,
+          ),
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Hello Sonia  👋',
+                        style: TextStyle(
+                            color: Color(0xFF0A1330),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: ClipOval(
+                          child: SizedBox.fromSize(
+                            size: Size.fromRadius(20), // Image radius
+                            child: Image.asset(
+                              'images/ff.jpeg',
+                              scale: 35,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 35.0),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 280.0,
-                      height: 50.0,
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.transparent,
-                              ),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(15.0),
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.transparent,
-                              ),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(15.0),
-                              ),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Color(0xFF78768E),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(15.0),
-                              ),
-                            ),
-                            filled: true,
-                            hintStyle: TextStyle(color: Colors.grey),
-                            hintText: "Search",
-                            fillColor: Color(0xFFF2F2F2)),
-                      ),
-                    ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: Container(
+                    ],
+                  ),
+                  SizedBox(height: 35.0),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 280.0,
                         height: 50.0,
-                        width: 45.0,
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: Icon(
-                          Icons.settings_input_composite_outlined,
-                          color: Colors.white,
+                        child: TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.transparent,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15.0),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.transparent,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15.0),
+                                ),
+                              ),
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Color(0xFF78768E),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15.0),
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Search",
+                              fillColor: Color(0xFFF2F2F2)),
                         ),
                       ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 30.0),
-                Text(
-                  'Breaking news',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20,
-                      color: Color(0xFF0A1330)),
-                ),
-                SizedBox(height: 18.0),
-               BreakingNewsScroll(
-               pageController: _pageController,
-                news:hotNews,
-               ),
-
-              ],
-            ),
-          ],
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: Container(
+                          height: 50.0,
+                          width: 45.0,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(15.0)),
+                          child: Icon(
+                            Icons.settings_input_composite_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 30.0),
+                  Text(
+                    'Breaking news',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        color: Color(0xFF0A1330)),
+                  ),
+                  SizedBox(height: 18.0),
+                  BreakingNewsScroll(
+                    pageController: _pageController,
+                    news: hotNews,
+                  ),
+                  SizedBox(height: 20.0),
+                 TabBar(
+                  unselectedLabelColor: Colors.grey,
+                  labelPadding: EdgeInsets.only(top: 5.0,left: 10.0,right: 10.0),
+                  isScrollable: true,
+                  controller: _tabController,
+                  labelColor: Colors.blue,
+                  indicator: PointTabIndicator(
+                   position: PointTabIndicatorPosition.top,
+                   color: Colors.blue,
+                   //insets: EdgeInsets.only(bottom: 6),
+                  ),
+                  tabs: tabList.map((item) {
+                   return Tab(
+                    text: item,
+                   );
+                  }).toList(),
+                 ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
