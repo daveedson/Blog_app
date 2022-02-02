fe78db370e09cbc5f5492b1b7e3129b1df4a95c6
David Onoh <daveeddrumz@gmail.com>
2022-02-02 10:40:25 +0100

    worked on the create post service for creating new posts

---
+++ lib/services/api_path_to_fireStore_database.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: file_names
+
+class ApiPath {
+  static String post(String uid, String postId) => "/users/$uid/posts/$postId";
+}
--- lib/services/create_post_service.dart
+++ lib/services/create_post_service.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:twinku_blog/models/createPost_model.dart';
+import 'package:twinku_blog/services/api_path_to_fireStore_database.dart';
 import 'package:twinku_blog/services/dataBase.dart';
 
 class CreatePostService implements DataBase {
@@ -8,6 +10,13 @@ final String? uid;
 
   @override
   Future<void> createPost(CreatePost createPost) async {
+   //gets the path to firestore database
+   final path = ApiPath.post(uid!, 'post_abc');
+   //gets the documents reference in firestore database
+   final documentReference = FirebaseFirestore.instance.doc(path);
+
+   //creates the data in cloud firestore
+   documentReference.set(createPost.toMap());
 
   }
 }
